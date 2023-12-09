e7022c68d35465524b19530bdacc2cb20206002a
若海 <wang@rehiy.com>
2023-12-09 19:42:44 +0800

    docs: 更新接口文档

--- httpd/server.go
+++ httpd/server.go
@@ -9,9 +9,9 @@ import (
 )
 
 // @title Wechat Rest API
-// @version v0.0.1
+// @version v0.4.0
 // @description 基于 WeChatFerry RPC 实现的电脑版微信 REST-API，使用 Go 语言编写，无第三方运行时依赖。基于 HTTP 提供操作接口，轻松对接任意编程语言。
-// @contact.name OpenTDP
+// @contact.name Github
 // @contact.url https://github.com/opentdp/wechat-rest
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
