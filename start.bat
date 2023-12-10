c0d40774971749cf649d36c62fb138a67d8d1487
若海 <wang@rehiy.com>
2023-12-10 12:00:43 +0800

    chore: 删除冗余变量

--- start.bat
+++ start.bat
@@ -6,9 +6,6 @@ SET GOARCH=386
 SET CGO_ENABLED=0
 SET GO111MODULE=on
 
-SET SDIR=%~dp0
-SET PATH=%PATH%;%SDIR%wcf-bin
-
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 CD /d %~dp0
