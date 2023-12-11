b15f91602765b764596edddb6db4f3f3e380e8a8
若海 <wang@rehiy.com>
2023-12-11 10:15:11 +0800

    fix: 修复消息打印格式

--- wcferry/pb_helper.go
+++ wcferry/pb_helper.go
@@ -2,10 +2,37 @@ package wcferry
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/opentdp/go-helper/request"
 )
+
+// 打印接收到的消息
+// param msg *WxMsg 消息
+func MsgPrinter(msg *WxMsg) {
+	re := regexp.MustCompile(`(?m)^\s*|\n`)
+	fmt.Print("\n=== New Message ===\n")
+	if msg.Id > 0 {
+		fmt.Printf("::Id:: %d\n", msg.Id)
+	}
+	if msg.Type > 0 {
+		fmt.Printf("::Type:: %d\n", msg.Type)
+	}
+	if msg.Roomid != "" {
+		fmt.Printf("::Roomid:: %s\n", msg.Roomid)
+	}
+	if msg.Sender != "" {
+		fmt.Printf("::Sender:: %v\n", msg.Sender)
+	}
+	if msg.Content != "" {
+		fmt.Printf("::Content:: %s\n", re.ReplaceAllString(msg.Content, ""))
+	}
+	if msg.Extra != "" {
+		fmt.Printf("::Extra:: %s\n", re.ReplaceAllString(msg.Extra, ""))
+	}
+	fmt.Print("=== End Message ===\n")
+}
 
 // 获取网络文件
 // param url string 文件URL或路径
@@ -17,29 +44,4 @@ func DownloadFile(url string) string {
 		}
 	}
 	return ""
-}
-
-// 打印接收到的消息
-// param msg *WxMsg 消息
-func MsgPrinter(msg *WxMsg) {
-	fmt.Print("=== New Message ===\n")
-	if msg.Id > 0 {
-		fmt.Printf("<<Id>> %d\n", msg.Id)
-	}
-	if msg.Type > 0 {
-		fmt.Printf("<<Type>> %d\n", msg.Type)
-	}
-	if msg.Roomid != "" {
-		fmt.Printf("<<Roomid>> %s\n", msg.Roomid)
-	}
-	if msg.Sender != "" {
-		fmt.Printf("<<Sender>> %v\n", msg.Sender)
-	}
-	if msg.Content != "" {
-		fmt.Printf("<<Content>> %s\n", msg.Content)
-	}
-	if msg.Extra != "" {
-		fmt.Printf("<<Extra>> %s\n", strings.TrimSpace(msg.Extra))
-	}
-	fmt.Print("=== End Message ===\n")
 }
