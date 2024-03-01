581690d9f4112aba16355b3ae424f60d5d9a9fd7
若海 <wang@rehiy.com>
2024-03-01 22:36:14 +0800

    feat: 添加全局配置列表

---
+++ webview/src/apps/setting/list.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { RobotApi, TablesSetting } from '../../openapi/wrobot';
+
+
+@Component({
+    selector: 'page-setting-list',
+    templateUrl: 'list.html',
+    styleUrls: ['list.scss']
+})
+export class SettingListComponent {
+
+    public settings: Array<TablesSetting> = [];
+
+    constructor() {
+        this.getSettings();
+    }
+
+    public getSettings() {
+        RobotApi.settingList({}).then((data) => {
+            this.settings = data || [];
+        });
+    }
+
+}
