1eb4bc96015c03b32feb27bc6e493237f6a890b0
若海 <wang@rehiy.com>
2024-03-02 14:47:59 +0800

    fix: 修复错误信息报告

--- webview/src/openapi/request.ts
+++ webview/src/openapi/request.ts
@@ -1,19 +1,23 @@
-export function httpRequest(input: string, options: RequestInit = {}) {
+export async function httpRequest(input: string, options: RequestInit = {}) {
     options.headers = Object.assign({ 'Content-Type': 'application/json' }, options.headers);
 
-    return fetch(input, options).then(response => {
-        if (response.status >= 200 && response.status < 300) {
-            return response.json().then(data => {
-                if (data.Error) {
-                    throw data.Error;
-                }
-                if (data.Message) {
-                    window.postMessage(data.Message);
-                }
-                return data.Payload;
-            });
-        } else {
-            throw response;
+    try {
+        const response = await fetch(input, options);
+        if (response.status < 200 || response.status > 300) {
+            throw new Error(response.statusText);
         }
-    });
+        const data = await response.json();
+        if (data.Message) {
+            window.postMessage(data.Message);
+        }
+        if (data.Error) {
+            throw data.Error;
+        }
+        return data.Payload;
+    } catch (error) {
+        const message = String(error);
+        window.postMessage({ message, classname: 'bg-danger text-light' });
+        throw error;
+    }
+
 }
