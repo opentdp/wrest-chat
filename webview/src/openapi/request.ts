059f5644df1ef6b4da00a013c4fb2e9e684a78e2
若海 <wang@rehiy.com>
2024-03-05 15:34:51 +0800

    feat: 请求支持携带token

--- webview/src/openapi/request.ts
+++ webview/src/openapi/request.ts
@@ -1,7 +1,15 @@
 export async function httpRequest(input: string, options: RequestInit = {}) {
-    const headers = { 'Content-Type': 'application/json' };
+    const headers: Record<string, string> = {
+        'Content-Type': 'application/json'
+    };
+    // 设置 Token
+    const token = sessionStorage.getItem('token');
+    if (token && token.trim().length > 1) {
+        headers['Authorization'] = `Bearer ${token}`;
+    }
+    // 设置 Header
     options.headers = Object.assign(headers, options.headers);
-
+    // 发起 HTTP 请求
     try {
         const response = await fetch(input, options);
         if (response.status < 200 || response.status > 300) {
@@ -12,6 +20,9 @@ export async function httpRequest(input: string, options: RequestInit = {}) {
             window.postMessage({ message: data.Message, type: 'success' });
         }
         if (data.Error) {
+            if (data.Error.Message) {
+                throw new Error(data.Error.Message);
+            }
             throw data.Error;
         }
         return data.Payload;
