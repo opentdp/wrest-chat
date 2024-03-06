eaf911d76c116d54d7d65cbec970966224b84794
若海 <wang@rehiy.com>
2024-03-06 18:11:21 +0800

    style: 合并消息字段

--- wcferry/types/msg_xml_atuser.go
+++ wcferry/types/msg_xml_atuser.go
@@ -1,16 +1,15 @@
 package types
 
 // 群里At用户消息
+// @type 1
 // @field Msg.Xml
 
 type AtMsgSource struct {
-	AtUserList  string  `xml:"atuserlist"`
-	Silence     int32   `xml:"silence"`
-	MemberCount int32   `xml:"membercount"`
-	Signature   string  `xml:"signature"`
-	TmpNode     TmpNode `xml:"tmp_node"`
-}
-
-type TmpNode struct {
-	PublisherID string `xml:",chardata"`
+	AtUserList  string `xml:"atuserlist"`
+	Silence     int32  `xml:"silence"`
+	MemberCount int32  `xml:"membercount"`
+	Signature   string `xml:"signature"`
+	TmpNode     struct {
+		PublisherID string `xml:",chardata"`
+	} `xml:"tmp_node"`
 }
