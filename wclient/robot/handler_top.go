264a685d500de525ee10e3ab8213019bedfbba04
若海 <wang@rehiy.com>
2024-03-06 08:58:06 +0800

    perf: 优化榜单生成模板

--- wclient/robot/handler_top.go
+++ wclient/robot/handler_top.go
@@ -2,7 +2,6 @@ package robot
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/opentdp/wechat-rest/dbase/message"
 	"github.com/opentdp/wechat-rest/wcferry"
@@ -17,20 +16,24 @@ func topHandler() {
 		RoomAble: true,
 		Describe: "获取群聊统计信息",
 		Callback: func(msg *wcferry.WxMsg) string {
-			text := []string{}
+			res := ""
 			// 聊天统计
-			text = append(text, "", "今日灌水排行", "----------------")
-			for _, v := range message.TalkTop10(msg.Roomid) {
-				u := wc.CmdClient.GetAliasInChatRoom(v.Sender, msg.Roomid)
-				text = append(text, fmt.Sprintf("%s:   %d 次", u, v.RecordCount))
+			if items := message.TalkTop10(msg.Roomid); len(items) > 0 {
+				res += "\n今日水王\n----------------\n"
+				for _, v := range items {
+					u := wc.CmdClient.GetAliasInChatRoom(v.Sender, msg.Roomid)
+					res += fmt.Sprintf("%s:   %d 次\n", u, v.RecordCount)
+				}
 			}
 			// 图片统计
-			text = append(text, "", "今日斗图排行", "----------------")
-			for _, v := range message.ImageTop10(msg.Roomid) {
-				u := wc.CmdClient.GetAliasInChatRoom(v.Sender, msg.Roomid)
-				text = append(text, fmt.Sprintf("%s:   %d 张", u, v.RecordCount))
+			if items := message.ImageTop10(msg.Roomid); len(items) > 0 {
+				res += "\n今日图王\n----------------\n"
+				for _, v := range items {
+					u := wc.CmdClient.GetAliasInChatRoom(v.Sender, msg.Roomid)
+					res += fmt.Sprintf("%s:   %d 次\n", u, v.RecordCount)
+				}
 			}
-			return strings.Join(text, "\n")
+			return res
 		},
 	}
 
