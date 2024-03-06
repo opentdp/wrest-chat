84aebec0b73830b459da040286589ebe0fe9d1e0
若海 <wang@rehiy.com>
2024-03-06 15:46:28 +0800

    perf: 重写gemini调用实现，减少大量依赖包

---
+++ library/google/typing.go
@@ -0,0 +1,49 @@
+package google
+
+type Client struct {
+	ApiBaseUrl string
+	ApiVersion string
+	ApiKey     string
+	Model      string
+}
+
+type RequestBody struct {
+	Contents []*Content `json:"contents"`
+}
+
+type ResponseBody struct {
+	Candidates     []*Candidate `json:"candidates"`
+	Error          *Error       `json:"error"`
+	PromptFeedback struct {
+		BlockReason   string `json:"blockReason"`
+		SafetyRatings []struct {
+			Category    string `json:"category"`
+			Probability string `json:"probability"`
+		} `json:"safetyRatings"`
+	} `json:"promptFeedback"`
+}
+
+type Content struct {
+	Parts []*Part `json:"parts"`
+	Role  string  `json:"role"`
+}
+
+type Part struct {
+	Text string `json:"text"`
+}
+
+type Error struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
+
+type Candidate struct {
+	Content       *Content `json:"content"`
+	FinishReason  string   `json:"finishReason"`
+	Index         int      `json:"index"`
+	SafetyRatings []*struct {
+		Category    string `json:"category"`
+		Probability string `json:"probability"`
+	} `json:"safetyRatings"`
+}
