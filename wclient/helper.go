f3a660390d8c15e467dcfdd65dde9934cc994f68
若海 <wang@rehiy.com>
2024-03-07 23:40:08 +0800

    refactor: 更改消息统计函数所在包

--- wclient/helper.go
+++ wclient/helper.go
@@ -1,4 +1,4 @@
-package message
+package wclient
 
 import (
 	"time"
--- wclient/robot/handler_top.go
+++ wclient/robot/handler_top.go
@@ -3,8 +3,8 @@ package robot
 import (
 	"fmt"
 
-	"github.com/opentdp/wechat-rest/dbase/message"
 	"github.com/opentdp/wechat-rest/wcferry"
+	"github.com/opentdp/wechat-rest/wclient"
 )
 
 func topHandler() {
@@ -18,7 +18,7 @@ func topHandler() {
 		Callback: func(msg *wcferry.WxMsg) string {
 			res := ""
 			// 聊天统计
-			if items := message.TalkTop10(msg.Roomid); len(items) > 0 {
+			if items := wclient.TalkTop10(msg.Roomid); len(items) > 0 {
 				res += "\n🏊 今日水王\n----------------\n"
 				for _, v := range items {
 					u := wc.CmdClient.GetAliasInChatRoom(v.Sender, msg.Roomid)
@@ -26,7 +26,7 @@ func topHandler() {
 				}
 			}
 			// 图片统计
-			if items := message.ImageTop10(msg.Roomid); len(items) > 0 {
+			if items := wclient.ImageTop10(msg.Roomid); len(items) > 0 {
 				res += "\n🌅 今日图王\n----------------\n"
 				for _, v := range items {
 					u := wc.CmdClient.GetAliasInChatRoom(v.Sender, msg.Roomid)
