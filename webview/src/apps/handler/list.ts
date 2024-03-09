4d70f850f71750913f3f741ef218cbf5ded7f833
若海 <wang@rehiy.com>
2024-03-09 23:47:53 +0800

    perf: 清理冗余代码

--- webview/src/apps/handler/list.ts
+++ webview/src/apps/handler/list.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 import { RobotApi, RobotHandler } from '../../openapi/wrobot';
 
@@ -12,8 +11,7 @@ export class HandlerListComponent {
 
     public robotHandler: Array<RobotHandler> = [];
 
-
-    constructor(private router: Router) {
+    constructor() {
         this.getRobotHandlers();
     }
 
