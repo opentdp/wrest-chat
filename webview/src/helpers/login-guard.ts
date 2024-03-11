891729baf5474157bd9d133aad5d45aa87cbd919
若海 <wang@rehiy.com>
2024-03-11 14:25:56 +0800

    fix: 未登录时跳转到首页

--- webview/src/helpers/login-guard.ts
+++ webview/src/helpers/login-guard.ts
@@ -3,7 +3,11 @@ import { CanActivateFn } from '@angular/router';
 import { WrestApi } from '../openapi/wcfrest';
 
 
-export const LoginGuard: CanActivateFn = () => {
-   return WrestApi.isLogin();
-};
+export const LoginGuard: CanActivateFn = async () => {
 
+   const isLogin = await WrestApi.isLogin();
+
+   isLogin || location.assign('/#/welcome');
+   return isLogin;
+
+};
