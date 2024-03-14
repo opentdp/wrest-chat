187c59716f9d5a55eaa72f0cf4dd35284868cb2a
若海 <wang@rehiy.com>
2024-03-15 00:41:39 +0800

    feat: 支持自动加载插件

--- wclient/robot/handler.go
+++ wclient/robot/handler.go
@@ -45,6 +45,7 @@ func ResetHandlers() {
 	hlst = append(hlst, banHandler()...)
 	hlst = append(hlst, cmddHandler()...)
 	hlst = append(hlst, helpHandler()...)
+	hlst = append(hlst, pluginHandler()...)
 	hlst = append(hlst, revokeHandler()...)
 	hlst = append(hlst, roomHandler()...)
 	hlst = append(hlst, topHandler()...)
