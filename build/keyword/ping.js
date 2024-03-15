13ae7e826f2505e3207d1c5210bb713b2e3ef398
若海 <wang@rehiy.com>
2024-03-15 16:59:25 +0800

    chore: 更新示例插件

---
+++ build/keyword/ping.js
@@ -0,0 +1,10 @@
+// 这是一个自定义指令插件，用于测试自定义指令能否正常工作
+// 参数中的 Rd 不要重复，且导入后不可随意修改，否则将影响插件的更新
+// @Rd: 100
+// @Roomid: *
+// @Phrase: ping
+// @Level: 9
+// @Target: node.exe
+// @Remark: 测试插件
+
+console.log('pong');
