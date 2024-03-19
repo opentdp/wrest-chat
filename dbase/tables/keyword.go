65c779c36162ba7347242484d40dc745eb730c83
若海 <wang@rehiy.com>
2024-03-19 11:42:50 +0800

    fix: 规范联合索引名称

--- dbase/tables/keyword.go
+++ dbase/tables/keyword.go
@@ -3,13 +3,13 @@ package tables
 // 关键词
 
 type Keyword struct {
-	Rd        uint   `json:"rd" gorm:"primaryKey"`                           // 主键
-	Group     string `json:"group" gorm:"uniqueIndex:t_r_phrase"`            // 分组
-	Roomid    string `json:"roomid" gorm:"uniqueIndex:t_r_phrase;default:-"` // 群聊 id
-	Phrase    string `json:"phrase" gorm:"uniqueIndex:t_r_phrase"`           // 短语
-	Level     int32  `json:"level" gorm:"default:-1"`                        // 等级
-	Target    string `json:"target"`                                         // 目标
-	Remark    string `json:"remark"`                                         // 备注
-	CreatedAt int64  `json:"created_at"`                                     // 创建时间戳
-	UpdatedAt int64  `json:"updated_at"`                                     // 最后更新时间戳
+	Rd        uint   `json:"rd" gorm:"primaryKey"`                                  // 主键
+	Group     string `json:"group" gorm:"uniqueIndex:idx_keyword_g_r_p"`            // 分组
+	Roomid    string `json:"roomid" gorm:"uniqueIndex:idx_keyword_g_r_p;default:-"` // 群聊 id
+	Phrase    string `json:"phrase" gorm:"uniqueIndex:idx_keyword_g_r_p"`           // 短语
+	Level     int32  `json:"level" gorm:"default:-1"`                               // 等级
+	Target    string `json:"target"`                                                // 目标
+	Remark    string `json:"remark"`                                                // 备注
+	CreatedAt int64  `json:"created_at"`                                            // 创建时间戳
+	UpdatedAt int64  `json:"updated_at"`                                            // 最后更新时间戳
 }
--- dbase/tables/profile.go
+++ dbase/tables/profile.go
@@ -3,13 +3,13 @@ package tables
 // 用户配置
 
 type Profile struct {
-	Rd        uint   `json:"rd" gorm:"primaryKey" `                      // 主键
-	Wxid      string `json:"wxid" gorm:"uniqueIndex:r_wxid"`             // 微信 id
-	Roomid    string `json:"roomid" gorm:"uniqueIndex:r_wxid;default:-"` // 群聊 id
-	Level     int32  `json:"level" gorm:"default:-1"`                    // 等级 [1:待验证, 2:已注册, 7:管理员, 9:创始人]
-	Remark    string `json:"remark"`                                     // 备注
-	AiModel   string `json:"ai_model"`                                   // 会话模型
-	BanExpire int64  `json:"ban_expire"`                                 // 拉黑截止时间
-	CreatedAt int64  `json:"created_at"`                                 // 创建时间戳
-	UpdatedAt int64  `json:"updated_at"`                                 // 最后更新时间戳
+	Rd        uint   `json:"rd" gorm:"primaryKey" `                               // 主键
+	Wxid      string `json:"wxid" gorm:"uniqueIndex:idx_profile_w_r"`             // 微信 id
+	Roomid    string `json:"roomid" gorm:"uniqueIndex:idx_profile_w_r;default:-"` // 群聊 id
+	Level     int32  `json:"level" gorm:"default:-1"`                             // 等级 [1:待验证, 2:已注册, 7:管理员, 9:创始人]
+	Remark    string `json:"remark"`                                              // 备注
+	AiModel   string `json:"ai_model"`                                            // 会话模型
+	BanExpire int64  `json:"ban_expire"`                                          // 拉黑截止时间
+	CreatedAt int64  `json:"created_at"`                                          // 创建时间戳
+	UpdatedAt int64  `json:"updated_at"`                                          // 最后更新时间戳
 }
