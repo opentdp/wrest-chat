e72b7f4e1253a32b569df181720f05ac81502ac8
若海 <wang@rehiy.com>
2024-04-08 15:24:47 +0800

    feat: 数据库文件写入Web.Storage

--- dbase/connect.go
+++ dbase/connect.go
@@ -2,17 +2,23 @@ package dbase
 
 import (
 	"github.com/opentdp/go-helper/dborm"
+	"github.com/opentdp/go-helper/filer"
 
+	"github.com/opentdp/wrest-chat/args"
 	"github.com/opentdp/wrest-chat/dbase/setting"
 	"github.com/opentdp/wrest-chat/dbase/tables"
 )
 
 func Connect() {
 
+	dbname := "wrest.db3"
+	if !filer.Exists(dbname) {
+		dbname = args.Web.Storage + "/" + dbname
+	}
+
 	// 连接数据库
 	db := dborm.Connect(&dborm.Config{
-		Type:   "sqlite",
-		DbName: "wrest.db3",
+		Type: "sqlite", DbName: dbname,
 	})
 
 	// 开启外键约束
