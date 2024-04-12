4a989b532bf8accaa800b6d1b492a866f585d414
dydhyhwu <dydhyh@foxmail.com>
2024-04-13 00:13:52 +0800

    feat(webview): add target_name, target_type on webhook list

--- webview/src/apps/webhook/list.ts
+++ webview/src/apps/webhook/list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import {RobotApi, Webhook} from '../../openapi/wrobot';
+import {WcfrestContactPayload, WrestApi} from "../../openapi/wcfrest";
 
 
 @Component({
@@ -10,8 +11,14 @@ import {RobotApi, Webhook} from '../../openapi/wrobot';
 export class WebhookListComponent {
     public list: Array<Webhook> = [];
 
+    public wcfContacts: Record<string, WcfrestContactPayload> = {};
+    public wcfChatrooms: Record<string, WcfrestContactPayload> = {};
+    public wcfRoomMembers: Record<string, Record<string, WcfrestContactPayload>> = {};
+
     constructor() {
         this.getWebhookList();
+        this.getWcfContacts();
+        this.getWcfChatrooms();
     }
 
     public getWebhookList() {
@@ -23,7 +30,31 @@ export class WebhookListComponent {
     public deleteWebhook(item: Webhook) {
         return RobotApi.webhookDelete({ rd: item.rd }).then(() => {
             this.getWebhookList();
-        })
+        });
+    }
+
+    public getWcfContacts() {
+        return WrestApi.contacts().then((data) => {
+            data.forEach((item) => this.wcfContacts[item.wxid] = item);
+        });
+    }
+
+    public getWcfChatrooms() {
+        return WrestApi.chatrooms().then((data) => {
+            data.forEach((item) => this.wcfChatrooms[item.wxid] = item);
+        });
+    }
+
+    public getWcfRoomMembers(id: string) {
+        if (this.wcfRoomMembers[id]) {
+            return Promise.resolve(); //已获取
+        }
+        this.wcfRoomMembers[id] = {}; //初始化
+        return WrestApi.chatroomMembers({ roomid: id }).then((data) => {
+            data && data.forEach((item) => {
+                this.wcfRoomMembers[id][item.wxid] = item;
+            });
+        });
     }
 
 }
